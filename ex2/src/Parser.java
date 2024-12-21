63bbb2b9d6aa1aee3caacb8a59ec37ca1c493842
Shahar Glam <shahar@Shahars-MacBook-Air.local>
2024-12-21 16:42:09 +0200

    fix(parser): write line error in output

--- ex2/src/Parser.java
+++ ex2/src/Parser.java
@@ -356,7 +356,7 @@ public class Parser extends java_cup.runtime.lr_parser {
 	public void report_error(String message, Object info)
 	{
 		
-		writer.write("Error");
+		writer.write("Error("+lexer.getLine() + ")");
 		writer.close();
 		System.exit(0);
 	}
