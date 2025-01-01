c5cc594223112c489b2059ae83e93e2df84bc97f
D4niel0s <das.volkov@gmail.com>
2025-01-01 13:34:20 +0200

    oopsie forgot types (too much python)

--- ex3/src/AST/AST_DEC_LIST.java
+++ ex3/src/AST/AST_DEC_LIST.java
@@ -5,8 +5,8 @@ public class AST_DEC_LIST extends AST_Node {
 	public AST_DEC value;
 	public AST_DEC_LIST next;
 
-	public AST_DEC_LIST(d, n) {
+	public AST_DEC_LIST(AST_DEC d, AST_DEC_LIST l) {
 		this.value = d;
-		this.next = n;
+		this.next = l;
 	}
 }
