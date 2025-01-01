9e73eb37e22086e9c2f486d574913258c3543864
D4niel0s <das.volkov@gmail.com>
2025-01-01 15:34:43 +0200

    new expression still unclear

--- ex3/src/AST/AST_NEW_EXP.java
+++ ex3/src/AST/AST_NEW_EXP.java
@@ -3,7 +3,11 @@ import TYPES.*;
 
 public class AST_NEW_EXP extends AST_Node /*TODO: determine inheritance*/ {
 
-	public AST_NEW_EXP(/*TODO*/) {
-	/*TODO: implement constructor(s?)*/
+	public AST_TYPE type;
+	public AST_EXP len;
+
+	public AST_NEW_EXP(AST_TYPE t, AST_EXP e) {
+		this.type = t;
+		this.len = e;
 	}
 }
