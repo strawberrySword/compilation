c476d374335e967e476b44fe702dee4bec1e747f
D4niel0s <das.volkov@gmail.com>
2025-01-01 13:07:52 +0200

    constructor for arglist

--- ex3/src/AST/AST_ARG_LIST.java
+++ ex3/src/AST/AST_ARG_LIST.java
@@ -3,7 +3,13 @@ import TYPES.*;
 
 public class AST_ARG_LIST extends AST_Node /*TODO: determine inheritance*/ {
 
-	public AST_ARG_LIST(/*TODO*/) {
-	/*TODO: implement constructor(s?)*/
+	public AST_TYPE type;
+	public String name;
+	public AST_ARG_LIST next;
+
+	public AST_ARG_LIST(AST_TYPE t, String n, AST_ARG_LIST l) {
+		this.type = t;
+		this.name = n;
+		this.next = l;
 	}
 }
