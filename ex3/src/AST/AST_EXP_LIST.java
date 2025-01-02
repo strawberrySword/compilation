91bf0483efda5326d9d8fadba6e31905aae485a1
D4niel0s <das.volkov@gmail.com>
2025-01-02 20:01:57 +0200

    Forgor printme

--- ex3/src/AST/AST_EXP_LIST.java
+++ ex3/src/AST/AST_EXP_LIST.java
@@ -16,6 +16,7 @@ public class AST_EXP_LIST extends AST_Node /*TODO: determine inheritance*/ {
 
 		if(this.next != null) this.next.PrintMe();
 
+		this.value.PrintMe();
 		AST_GRAPHVIZ.getInstance().logNode(this.SerialNumber, String.format("ExpList"));
 		AST_GRAPHVIZ.getInstance().logEdge(this.SerialNumber, this.value.SerialNumber);
 
