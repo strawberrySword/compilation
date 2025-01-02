a2a9e6a528d105a5d57fa0adeb35920fbced1864
D4niel0s <das.volkov@gmail.com>
2025-01-02 20:04:01 +0200

    Forgor printme

--- ex3/src/AST/AST_STMT_ASSIGN.java
+++ ex3/src/AST/AST_STMT_ASSIGN.java
@@ -15,6 +15,10 @@ public class AST_STMT_ASSIGN extends AST_STMT {
 	public void PrintMe(){
 		System.out.format("Stmt_asgn");
 
+
+		this.var.PrintMe();
+		this.exp.PrintMe();
+		
 		AST_GRAPHVIZ.getInstance().logNode(this.SerialNumber, String.format("Stmt_asgn"));
 		AST_GRAPHVIZ.getInstance().logEdge(this.SerialNumber, this.var.SerialNumber);
 		AST_GRAPHVIZ.getInstance().logEdge(this.SerialNumber, this.exp.SerialNumber);
