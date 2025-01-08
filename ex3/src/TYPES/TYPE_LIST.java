7c79bb0508c5c94357ebb452c104a83d4a3903c4
D4niel0s <das.volkov@gmail.com>
2025-01-08 21:41:25 +0200

    implementation of equals()

--- ex3/src/TYPES/TYPE_LIST.java
+++ ex3/src/TYPES/TYPE_LIST.java
@@ -16,4 +16,22 @@ public class TYPE_LIST extends TYPE
 		this.head = head;
 		this.tail = tail;
 	}
+
+	public boolean equals(TYPE_LIST other){
+		if (this.head != other.head){ // type mismatch
+			return false;
+		}
+
+		if (this.tail == null){
+			if (other.tail == null){ // both lists end
+				return true;
+			}else{ // only this list ended
+				return false;
+			}
+		}else if (other.tail == null){ // only other list ended
+			return false;
+		}
+
+		return this.tail.equals(other.tail);
+	}
 }
