0d7fa41532a427ec32fb3ec0088806681c569fbf
D4niel0s <das.volkov@gmail.com>
2025-01-08 14:28:37 +0200

    Inheritance checking for types

--- ex3/src/TYPES/TYPE.java
+++ ex3/src/TYPES/TYPE.java
@@ -1,5 +1,7 @@
 package TYPES;
 
+import SYMBOL_TABLE.SYMBOL_TABLE;
+
 public abstract class TYPE
 {
 	/******************************/
@@ -16,4 +18,20 @@ public abstract class TYPE
 	/* isArray() */
 	/*************/
 	public boolean isArray(){ return false;}
+
+	public boolean inheritsFrom(TYPE other){
+		if (!(this instanceof TYPE_CLASS) && !(other instanceof TYPE_CLASS)){ // two primitives
+			return this.name.equals(other.name);
+		}
+
+		if( (this instanceof TYPE_CLASS && !(other instanceof TYPE_CLASS)) || !(this instanceof TYPE_CLASS) && other instanceof TYPE_CLASS){ // one class one primitive
+			return false;
+		}
+
+		if (this.name == other.name){ // two identical types
+			return true;
+		}
+
+		return ((TYPE_CLASS)this).father.inheritsFrom(other);
+	}
 }
