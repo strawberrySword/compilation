0bbb1bde98a1511d1f489e7521fbe8fde4f7b4d3
D4niel0s <das.volkov@gmail.com>
2025-01-08 18:21:18 +0200

    type array

---
+++ ex3/src/TYPES/TYPE_ARRAY.java
@@ -0,0 +1,11 @@
+package TYPES;
+
+public class TYPE_ARRAY extends TYPE {
+    public String name;
+    public TYPE dataType;
+
+    public TYPE_ARRAY(String n, TYPE t){
+        this.name = n;
+        this.dataType = t;
+    }
+}
