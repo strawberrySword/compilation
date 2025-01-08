fb86cac78d5cf2abb843dac910ff6156ac680798
D4niel0s <das.volkov@gmail.com>
2025-01-08 14:07:51 +0200

    AST_DEC SemantMe()

--- ex3/src/AST/AST_VAR_DEC.java
+++ ex3/src/AST/AST_VAR_DEC.java
@@ -1,4 +1,5 @@
 package AST;
+import SYMBOL_TABLE.SYMBOL_TABLE;
 import TYPES.*;
 
 public class AST_VAR_DEC extends AST_DEC /*TODO: determine inheritance*/ {
@@ -26,5 +27,61 @@ public class AST_VAR_DEC extends AST_DEC /*TODO: determine inheritance*/ {
 	
 		if (this.exp != null) AST_GRAPHVIZ.getInstance().logEdge(this.SerialNumber, this.exp.SerialNumber);
 		if (this.newExp != null) AST_GRAPHVIZ.getInstance().logEdge(this.SerialNumber, this.newExp.SerialNumber);
+	}
+
+	public TYPE SemantMe(){
+		// type != void
+		// Name not taken
+		// type exists
+		// type matches with assignment (exp or newExp) (Check inheritance)
+		// if inside class, must be a constant exp!!!! (of type AST_INT/AST_STRING/AST_NIL)
+		
+		SYMBOL_TABLE sTable = SYMBOL_TABLE.getInstance();
+		
+
+		if (this.type.myType.equals("void")){ // variable declaration of type void
+			System.out.println("Semantic error: variable cannot be of type void");
+			System.exit(0);
+		}
+
+		if (sTable.find(this.name) != null){ // a variable with this name exists
+			System.out.println("Semantic error: variable name is taken");
+			System.exit(0);
+		}
+
+		if (sTable.find(this.type.myType) == null){ // type does not exist
+			System.out.println("Semantic error: type "+this.type.myType+" does not exist");
+			System.exit(0);
+		}
+
+		if ( (sTable.inClassDef() && this.newExp != null) || (sTable.inClassDef() && !(this.exp instanceof AST_INT) && !(this.exp instanceof AST_STRING) && !(this.exp instanceof AST_NIL))){ // variables in class definition must be assigned a constant expression
+			System.out.println("Semantic error: variable declaration inside class must be assigned a constant expression");
+			System.exit(0);
+		}
+
+
+		 // type of assignment must match declaration type 
+		TYPE tLeft = sTable.find(this.type.myType);
+		TYPE tRight;
+		if (this.newExp == null){
+			if (this.exp == null){ // both null "int x;"
+				sTable.enter(this.name, tLeft);
+				return tLeft;
+			}
+
+			tRight = this.exp.SemantMe(); // "int x := 32 + 98;"
+		}else{ // new assign
+			tRight = this.newExp.SemantMe(); // "Car x := new Car;"
+		}
+
+		if (!(tRight.inheritsFrom(tLeft))){ // Check inheritance
+			System.out.println("Semantic error: assignment type mismatch");
+			System.exit(0);
+		}
+
+		sTable.enter(this.name, tLeft);
+		return tLeft;
+		
+
 	}
 }
