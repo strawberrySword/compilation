2f1d9e20c7388682889e7a993ae8b17f6f66edac
D4niel0s <das.volkov@gmail.com>
2025-01-08 14:32:22 +0200

    forgot to end recursion

--- ex3/src/TYPES/TYPE.java
+++ ex3/src/TYPES/TYPE.java
@@ -23,15 +23,19 @@ public abstract class TYPE
 		if (!(this instanceof TYPE_CLASS) && !(other instanceof TYPE_CLASS)){ // two primitives
 			return this.name.equals(other.name);
 		}
-
 		if( (this instanceof TYPE_CLASS && !(other instanceof TYPE_CLASS)) || !(this instanceof TYPE_CLASS) && other instanceof TYPE_CLASS){ // one class one primitive
 			return false;
 		}
 
+		// Two class types
+
 		if (this.name == other.name){ // two identical types
 			return true;
 		}
+		if (((TYPE_CLASS)this).father == null){ // types not equal and this doesn't inherit from anything
+			return false;
+		}
 
-		return ((TYPE_CLASS)this).father.inheritsFrom(other);
+		return ((TYPE_CLASS)this).father.inheritsFrom(other); // recursive step up
 	}
 }
