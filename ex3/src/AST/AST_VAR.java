5c1c97ee2bec8cf25d05e069f3c6eda48c1efb2e
D4niel0s <das.volkov@gmail.com>
2025-01-09 14:49:44 +0200

    abstract SemantMe()

--- ex3/src/AST/AST_VAR.java
+++ ex3/src/AST/AST_VAR.java
@@ -1,8 +1,10 @@
 package AST;
+import TYPES.*;
 
 public abstract class AST_VAR extends AST_EXP{
 
 	/* Abstract class to allow for polymorphism */
 
 	public void PrintMe(){}
+	public TYPE SemantMe(){return null;}
 }
