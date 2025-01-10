3d76d4c1e54344e58af40f2c6b0c3ce0abd2f861
Shahar Glam <shahar@Shahars-Air.lan>
2025-01-10 14:06:51 +0200

    fixes

--- ex3/src/Main.java
+++ ex3/src/Main.java
@@ -52,7 +52,7 @@ public class Main
 			/**************************/
 			/* [7] Semant the AST ... */
 			/**************************/
-			AST.SemantMe();
+			AST.SemantMe(file_writer);
 			
 			/*************************/
 			/* [8] Close output file */
