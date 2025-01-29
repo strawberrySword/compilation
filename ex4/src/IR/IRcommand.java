a54bf65a1b6a78981dc80cd8b079fbd0e3310fc4
D4niel0s <das.volkov@gmail.com>
2025-01-29 21:31:55 +0200

    Computation of transfer functions. Need to implement computation of kill/gen sets.

--- ex4/src/IR/IRcommand.java
+++ ex4/src/IR/IRcommand.java
@@ -1,7 +1,48 @@
 package IR;
 
+import java.util.HashSet;
+
 public abstract class IRcommand
 {
+	public HashSet<assignment> in;
+    public HashSet<assignment> out;
+
+    public class assignment {
+        String var;
+        boolean isAssigned;
+
+        public assignment(String v, boolean b) {
+            this.var = v;
+            this.isAssigned = b;
+        }
+
+		public assignment copy(){
+			return new assignment(this.var, this.isAssigned);
+		}
+    }
+
+	public void computeAndAssignOutSetForEx4(){
+		out = new HashSet<>();
+
+		for (assignment a: in){
+			out.add(a.copy());
+		}
+
+		HashSet<assignment> killSet = this.computeKillSet();
+		HashSet<assignment> genSet= this.computeGenSet();
+
+		out.removeAll(killSet);
+		out.addAll(genSet);
+	}
+
+	// TODO: COMPUTE KILL & GEN SETS!
+	public HashSet<assignment> computeKillSet(){return null;}
+	public HashSet<assignment> computeGenSet(){return null;}
+
+	public void initInSet(){
+		this.in = new HashSet<>();
+	}
+
 	protected static int label_counter=0;
 	public    static String getFreshLabel(String msg)
 	{
