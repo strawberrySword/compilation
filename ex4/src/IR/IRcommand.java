feb4c9b45ca1c13be355c71e3c26844e4df6af68
D4niel0s <das.volkov@gmail.com>
2025-01-29 22:12:26 +0200

    Kill/Gen set calculation

--- ex4/src/IR/IRcommand.java
+++ ex4/src/IR/IRcommand.java
@@ -35,9 +35,68 @@ public abstract class IRcommand
 		out.addAll(genSet);
 	}
 
-	// TODO: COMPUTE KILL & GEN SETS!
-	public HashSet<assignment> computeKillSet(){return null;}
-	public HashSet<assignment> computeGenSet(){return null;}
+	// This computes kill set for this IRcommand object
+	public HashSet<assignment> computeKillSet(){
+
+		HashSet<assignment> res = new HashSet<>();
+
+		if (this instanceof IRcommandConstInt cInt){
+			res.add(new assignment(cInt.t.toString(), true));
+			res.add(new assignment(cInt.t.toString(), false));
+		}
+		else if (this instanceof IRcommand_Allocate alloc){
+			res.add(new assignment(alloc.var_name, true));
+			res.add(new assignment(alloc.var_name, false));
+		}
+		else if (this instanceof IRcommand_Load load){
+			res.add(new assignment(load.dst.toString(), true));
+			res.add(new assignment(load.dst.toString(), false));
+		}
+		else if (this instanceof IRcommand_Store store){
+			res.add(new assignment(store.var_name, true));
+			res.add(new assignment(store.var_name, false));
+		}
+		else if (this instanceof IRcommand_Binop binop){
+			res.add(new assignment(binop.dst.toString(), true));
+			res.add(new assignment(binop.dst.toString(), false));
+		}
+		return res;
+	}
+
+	// This computes gen set for this IRcommand object
+	public HashSet<assignment> computeGenSet(){
+		
+		HashSet<assignment> res = new HashSet<>();
+
+		if (this instanceof IRcommandConstInt cInt){
+			res.add(new assignment(cInt.t.toString(), true));
+		}
+		else if (this instanceof IRcommand_Allocate alloc){
+			res.add(new assignment(alloc.var_name, false));
+		}
+		else if (this instanceof IRcommand_Load load){
+			res.add(new assignment(load.dst.toString(), this.isInitialized(load.var_name)));
+		}
+		else if (this instanceof IRcommand_Store store){
+			res.add(new assignment(store.var_name, this.isInitialized(store.src.toString())));
+		}
+		else if (this instanceof IRcommand_Binop binop){
+			boolean ans = this.isInitialized(binop.t1.toString()) && this.isInitialized(binop.t2.toString());
+			res.add(new assignment(binop.dst.toString(), ans)); // will be true iff both are initialized
+		}
+		return res;
+	}
+
+	// This function determines wether a variable (or temp) is initialized at IN
+	private boolean isInitialized(String vName){
+		for (assignment a : this.in){
+			if (a.var.equals(vName)){
+				return a.isAssigned;
+			}
+		}
+
+		return false;
+	}
 
 	public void initInSet(){
 		this.in = new HashSet<>();
