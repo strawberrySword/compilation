794eb33051125ac9cb615d376365423c1613dad8
D4niel0s <das.volkov@gmail.com>
2025-01-29 21:30:40 +0200

    Added chaotic iterations implementation

--- ex4/src/IR/IR.java
+++ ex4/src/IR/IR.java
@@ -1,5 +1,10 @@
 package IR;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import IR.IRcommand.assignment;
+
 public class IR
 {
 	private IRcommand head=null;
@@ -67,6 +72,43 @@ public class IR
 		}
 
 		return cfg;
+	}
+
+	public void AnalyzeCFG(Graph<IRcommand> cfg, HashSet<assignment> initialValue){
+		for (IRcommand node : cfg.adjacencyList.keySet()) {
+			node.initInSet();
+    	}
+
+		IRcommand first = cfg.getFirstCommand();
+		IRcommand curr;
+
+		first.in = initialValue;
+
+		ArrayList<IRcommand> workList = new ArrayList<>();
+		ArrayList<IRcommand> next;
+
+		HashSet<assignment> temp;
+
+		workList.add(first);
+
+		while (!(workList.isEmpty())){
+			curr = workList.get(0); // take out the first command (doesn't matter which really)
+			workList.remove(0); // remove the command we're working on
+
+			curr.computeAndAssignOutSetForEx4(); // compute transfer function
+
+			next = (ArrayList<IRcommand>)cfg.getNeighbors(curr); // the "next" in the cfg
+
+			for (IRcommand n: next){
+				temp = n.in;
+
+				n.in.addAll(curr.out); // in_{v} = in_{v} U out_{u}
+
+				if (!(temp.equals(n.in))){
+					workList.add(n);
+				}
+			}
+		}
 	}
 
 	private IRcommand findNodeByLabel(String label){
