574b9dff6ab55f3cd542e4dbeb2c5a4360d49dee
D4niel0s <das.volkov@gmail.com>
2025-01-29 21:46:02 +0200

    Crucial: now extends IRcommand

--- ex4/src/IR/IRcommand_Binop.java
+++ ex4/src/IR/IRcommand_Binop.java
@@ -2,7 +2,7 @@ package IR;
 
 import TEMP.TEMP;
 
-public abstract class IRcommand_Binop {
+public abstract class IRcommand_Binop extends IRcommand{
     public TEMP t1;
 	public TEMP t2;
 	public TEMP dst;
